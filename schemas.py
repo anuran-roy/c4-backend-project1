1c3ab3495cc16a435739cecc40282f278522ed73
anuran <anuranroy02@gmail.com>
2021-09-26 04:22:55 +0530

    Added users feature to blog API

--- schemas.py
+++ schemas.py
@@ -5,3 +5,27 @@ class Blog(BaseModel):
     title: str
     description: str
     published: Optional[bool]
+
+class ShowBlog(Blog): 
+    class Config():
+        orm_mode = True
+
+class ShowList(BaseModel):
+    title: str
+
+    class Config():
+        orm_mode = True
+
+class User(BaseModel):
+    name: str
+    username: str
+    email: str
+    password: str
+
+class UserProfile(BaseModel):
+    name: str
+    email: str
+    username: str
+
+    class Config(): 
+        orm_mode = True
